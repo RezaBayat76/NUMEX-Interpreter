81ef6a2032816cc22c926604eac845383a5a71f2
reza <reza.bayat76@gmail.com>
2019-01-20 11:59:00 +0330

    problem 1 (Warm-Up): add tow function which one convert racketlist to numexlist and other one convert numexlist to racketlist

--- project.rkt
+++ project.rkt
@@ -41,8 +41,18 @@
 
 ;; Problem 1
 
-(define (racketlist->numexlist xs) "CHANGE")
-(define (numexlist->racketlist xs) "CHANGE")
+(define (racketlist->numexlist xs)
+  (cond [(null? xs) (munit)]
+        [(list? xs) (apair (car xs) (racketlist->numexlist (cdr xs)))]
+        [#t (error ("it's not a racket list"))]
+  )
+)
+(define (numexlist->racketlist xs)
+  (cond [(munit? xs) null]
+        [(apair? xs) (cons (apair-e1 xs) (numexlist->racketlist (apair-e2 xs)))]
+        [#t (error ("it's not a numex list"))]
+  )
+)
 
 ;; Problem 2
 
@@ -50,7 +60,7 @@
 ;; Complete this function
 (define (envlookup env str)
   (cond [(null? env) (error "unbound variable during evaluation" str)]
-  		"CHANGE" 
+  	;;	"CHANGE" 
 		)
  )
 
